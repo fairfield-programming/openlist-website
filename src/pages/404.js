c3a149e18db8020048414e5690fd49b2f61a2b8e
William McGonagle <wpmcgonagle@gmail.com>
2023-02-12 18:02:02 -0500

    Added 404 Page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,14 +2,20 @@ import * as React from "react"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import { Link } from "gatsby"
 
 const NotFoundPage = () => (
   <Layout>
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <section className="w-full h-full my-auto">
+      <h1 className="text-5xl font-bold text-center mt-48">Page Not Found</h1>
+      <p className="text-xl font-light text-center mx-auto max-w-xl mt-4">We couldn't find the page that you were looking for. It may have been deleted, or we could have just lost it. 🤷</p>
+      <p className="text-xl font-bold text-center mx-auto max-w-xl mt-16">
+        <Link class="text-transparent bg-clip-text bg-gradient-to-r from-primary to-secondary hover:cursor hover:underline" to="/">Go Back Home</Link>
+      </p>
+    </section>
   </Layout>
 )
 
-export const Head = () => <Seo title="Not Found" />
+export const Head = () => <Seo title="Page Not Found" />
 
 export default NotFoundPage
