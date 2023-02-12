c1cbd5ce50f76d9ef65bae742a545f9dc26750a9
William McGonagle <wpmcgonagle@gmail.com>
2023-02-12 09:06:02 -0500

    Added More Config

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,18 +16,43 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-postcss',
+    `gatsby-plugin-sharp`,
     `gatsby-plugin-image`,
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    // {
-    //   resolve: `gatsby-plugin-manifest`,
-    //   options: {
-    //     name: `gatsby-starter-default`,
-    //     short_name: `starter`,
-    //     start_url: `/`,
-    //     background_color: `#663399`,
-    //     display: `minimal-ui`,
-    //   },
-    // },
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        host: false,
+        sitemap: 'https://about.fairfieldprogramming.org/sitemap/sitemap-index.xml',
+        policy: [{ userAgent: '*', allow: '/' }]
+      }
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Fairfield Programming Association`,
+        short_name: `fpa`,
+        start_url: `/`,
+        background_color: `#0F006C`,
+        theme_color: `#0F006C`,
+        display: `minimal-ui`,
+        icon: `src/res/favicon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-plugin-breadcrumb`,
+      options: {
+        // defaultCrumb: optional To create a default crumb
+        // see Click Tracking default crumb example below
+        defaultCrumb: {
+          location: {
+            pathname: "/",
+          },
+          crumbLabel: "Home",
+          crumbSeparator: " /",
+        },
+      }
+    },
   ],
 }
