cab9fe3c7ea514c33639501db44d00d1199e3f55
Aditya Ghadge <adityaghadge360@gmail.com>
2023-02-28 07:12:38 +0530

    Remove the search section on the home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -48,15 +48,6 @@ const IndexPage = () => (
           </div>
         </div>
       </section>
-      <section className="max-w-5xl mt-16 p-4 mx-auto">
-        <h2 className="text-3xl font-black text-center">Looking for a specific license?</h2>
-        <div className="w-full h-24 relative mt-4">
-          <input placeholder="Search Here..." className="absolute pl-12 top-0 w-full shadow outline-0 p-4 rounded-2xl font-bold border-2 border-slate-50" />
-          <svg className="absolute w-5 top-5 left-5" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16">
-            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
-          </svg>
-        </div>
-      </section>
     </Layout>
 )
 
